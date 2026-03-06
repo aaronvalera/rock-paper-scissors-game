7031e1b2c721d7ad01df863647f3f63b4ae17e10
Aaron Valera <aaronrvalera@gmail.com>
2026-03-06 15:35:58 -0400

    Fix counters and overlay change
    Was added to the overlay choices' placeholders to display the decision
    made by both players, in order to improve the experience of the game.
    
    Adittionally, counters were fixed, due to the fact that they did not
    display the scores properly, generating confusion around the actual
    scores.
    
    Player buttons' size were reduced to get the focus on the choices'
    placeholders, scores, and round result.

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+// Variables
+let humanScore = 0;
+let computerScore = 0;
+let playedRounds = 0;
+
+// Button selectors
+const rockBtn = document.querySelector("#rock");
+rockBtn.addEventListener("click", () => {
+   playRound("Rock");
+   humanChoiceHolder.textContent = "💎";
+});
+
+const paperBtn = document.querySelector("#paper");
+paperBtn.addEventListener("click", () => {
+   playRound("Paper");
+   humanChoiceHolder.textContent = "📄";
+});
+
+const scissorsBtn = document.querySelector("#scissors");
+scissorsBtn.addEventListener("click", () => {
+   playRound("Scissors");
+   humanChoiceHolder.textContent = "✂️";
+});
+
+// Text selectors
+const humanChoiceHolder = document.querySelector(".player-choice");
+const computerChoiceHolder = document.querySelector(".computer-choice")
+const roundResult = document.querySelector("#roundResult");
+const finalResult = document.querySelector("#finalResult");
+const humanCounter = document.querySelector(".playerScore");
+const computerCounter = document.querySelector(".computerScore");
+
 // function to get a random rounded integer
 const getRandomInt = () => {
    return Math.floor(Math.random() * 3);
@@ -7,87 +39,19 @@ const getRandomInt = () => {
 const getComputerChoice = () => {
    let computerChoice = getRandomInt();
      if(computerChoice == 0) {
-         console.log("Rock");
-         return "Rock";
+        computerChoiceHolder.textContent = "💎";
+        return "Rock";
       } else if(computerChoice == 1) {
-         console.log("Paper");
+         computerChoiceHolder.textContent = "📄";
          return "Paper";
     } else {
-         console.log("Scissors");
+         computerChoiceHolder.textContent = "✂️";
          return "Scissors";
     }
 }
 
-const rockBtn = document.querySelector("#rock");
-rockBtn.addEventListener("click", () => {
-   playRound("Rock");
-});
-const paperBtn = document.querySelector("#paper");
-paperBtn.addEventListener("click", () => {
-   playRound("Paper");
-});
-const scissorsBtn = document.querySelector("#scissors");
-scissorsBtn.addEventListener("click", () => {
-   playRound("Scissors");
-});
-
-// function the get the human's choice
-const getHumanChoice = () => {
-   if(humanChoice1 == "Rock") {
-      console.log("Rock");
-         return "Rock";
-      } else if(humanChoice2 == "Paper") {
-         console.log("Paper");
-         return "Paper";
-      } else {
-         console.log("Scissors");
-         return "Scissors";
-    }
-   }
-
-let humanScore = 0;
-let computerScore = 0;
-let playedRounds = 0;
-const roundResult= document.querySelector("#roundResult");
-const finalResult = document.querySelector("#finalResult");
-const playerCounter = document.querySelector(".playerScore");
-const computerCounter = document.querySelector(".computerScore");
-
-// function to structure a round of the game
-const playRound = (humanChoice) => {
-   if(playedRounds >= 5) {
-      return;
-   }
-
-   playerCounter.innerHTML = `<p>Player: ${humanScore}</p>`;
-   computerCounter.innerHTML = `<p>Computer: ${computerScore}</p>`;
-   
-   const computerChoice = getComputerChoice();
-   if(computerChoice === humanChoice) {
-      roundResult.innerHTML = "<h2>It's a tie!</h2>";
-   } else if(
-      (computerChoice === "Rock" && humanChoice === "Scissors") ||
-         (computerChoice === "Paper" && humanChoice === "Rock") ||
-         (computerChoice === "Scissors" && humanChoice == "Paper")
-      ) {
-         roundResult.innerHTML = `<h2>You lost!</h2>
-         <p>${computerChoice} beats ${humanChoice}!</p>`;
-         computerScore++;
-      } else {
-         roundResult.innerHTML = `<h2>You win!</h2>
-         <p>${humanChoice} beats ${computerChoice}!</p>`;
-         humanScore++;
-      }
-
-   playedRounds++;
-
-   if(playedRounds === 5) {
-      declareWinner();
-   }
-}
-                           
    // conditional statement to declare the result of the game
-   const declareWinner = () => { 
+   const declareWinner = () => {
       if(humanScore > computerScore) {
          finalResult.textContent = `You win! Final score:
             Human Score: ${humanScore} - Computer Score: ${computerScore}`;
@@ -99,3 +63,39 @@ const playRound = (humanChoice) => {
             Human Score: ${humanScore} - Computer Score: ${computerScore}`;
       }
    }
+
+// function to structure a round of the game
+const playRound = (humanChoice) => {
+   if(humanScore === 5 || computerScore === 5) return;
+
+   roundResult.classList.remove("victory", "defeat", "tie");
+
+   const computerChoice = getComputerChoice();
+   if(computerChoice === humanChoice) {
+      roundResult.innerHTML = "<h2>It's a tie!</h2>";
+      roundResult.classList.add("tie");
+   } else if(
+      (computerChoice === "Rock" && humanChoice === "Scissors") ||
+      (computerChoice === "Paper" && humanChoice === "Rock") ||
+      (computerChoice === "Scissors" && humanChoice == "Paper")
+   ) {
+         roundResult.innerHTML = `<h2>You lost!</h2>
+         <p>${computerChoice} beats ${humanChoice}!</p>`;
+         roundResult.classList.add("defeat");
+         computerScore++;
+      } else {
+         roundResult.innerHTML = `<h2>You win!</h2>
+         <p>${humanChoice} beats ${computerChoice}!</p>`;
+         roundResult.classList.add("victory");
+         humanScore++;
+      }
+
+      playedRounds++;
+
+      humanCounter.innerHTML = `<p>Player: ${humanScore}</p>`;
+      computerCounter.innerHTML = `<p>Computer: ${computerScore}</p>`;
+
+   if(humanScore === 5 || computerScore === 5) {
+      declareWinner();
+   }
+}
